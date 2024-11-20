2343027467493b1ab2499a81442958fcc2147600
Yuriy Surniak <surniakyura@gmail.com>
2024-11-21 00:16:04 +0200

    Yurii added

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/YuraAvatar.jpg"} alt="Yurii Surniak" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Yurii Surniak</h2>
+          <p style={styles.position}>Team Lead</p>
         </div>
       </div>
     </main>
