563b1ff249330250f7eb6c2b41aeeeb357f6cc76
teloor <andrii.chornyi.pz.2023@lpnu.ua>
2024-11-20 23:10:55 +0200

    Chornyi added

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+              <img src={"/chornyiAvatar.jpg"} alt="Andrii Chornyi" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Andrii Chornyi</h2>
+          <p style={styles.position}>UI/UX Designer</p>
         </div>
       </div>
     </main>
